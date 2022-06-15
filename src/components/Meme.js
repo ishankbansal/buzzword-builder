1ef95cd4829d9c64e08a92067839382033f19269
Ishank Bansal <ishankbansal1111@gmail.com>
2022-06-15 23:59:29 +0530

    day 2

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,28 @@
+import memesData from "../memedata.js"
 export default function Meme(){
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+    }
     return(
         <main>
-            <form className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button">Get a new meme image 🏞</button>
-            </form>
+            <div className="form">
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Top text"
+                />
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Bottom text"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                    >Get a new meme image 🏞</button>
+            </div>
         </main>
     )
 }
