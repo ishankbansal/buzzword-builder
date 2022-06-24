a8bffd62de6b18e766653156f3b5817e984c0f79
Ishank Bansal <ishankbansal1111@gmail.com>
2022-06-24 23:59:47 +0530

    changes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,11 @@
 import memesData from "../memedata.js"
+import React from 'react'
 export default function Meme(){
     const [memeImage, setMemeImage] = React.useState("")
     function getMemeImage(){
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        setMemeImage(memesArray[randomNumber].url)
     }
     return(
         <main> 
@@ -24,6 +25,7 @@ export default function Meme(){
                     onClick={getMemeImage}
                     >Get a new meme image 🏞</button>
             </div>
+            <img src={memeImage} className="meme--image"/>
         </main>
     )
 }
