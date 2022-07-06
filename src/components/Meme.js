b46a99de747b08869f4f9976a890fed6d07ba0ab
Ishank Bansal <ishankbansal1111@gmail.com>
2022-07-06 18:27:24 +0530

    one step closer

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -21,6 +21,15 @@ export default function Meme(){
             randomImage: url
         }))
     }
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return(
         <main> 
             <div className="form">
@@ -28,18 +37,28 @@ export default function Meme(){
                     type="text" 
                     className="form--input" 
                     placeholder="Top text"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text" 
                     className="form--input" 
                     placeholder="Bottom text"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button 
                     className="form--button"
                     onClick={getMemeImage}
                     >Get a new meme image 🏞</button>
             </div>
-            <img src={meme.randomImage} className="meme--image"/>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
