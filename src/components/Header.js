eeef2dba628c5af18b2691ea8b26d4d6dbc813d9
Ishank Bansal <ishankbansal1111@gmail.com>
2022-07-28 20:18:08 +0530

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <header className="header">
             <img src={trollFace} alt="" className="header--image"/>
-            <h2 className="header--title">Meme Generator</h2>
+            <h2 className="header--title">Buzzword Builder</h2>
             <h4 className="header--project">Made with 🤍 by Ishank</h4>
         </header>
     )
