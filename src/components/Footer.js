728426d140f86be75a8d3ec05050f885a4f12c9d
Ishank Bansal <ishankbansal1111@gmail.com>
2022-07-29 19:26:59 +0530

    adding footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer(){
+    return(
+        <footer className="footer">
+            <h3>Made with 🤍 by Ishank</h3>
+        </footer>
+    )
+}
