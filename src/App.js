82f49cd66d9a61bb9b8e75dfb831269291c6c66e
Ishank Bansal <ishankbansal1111@gmail.com>
2022-07-29 19:27:59 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Header/>
       <Meme/>
+      <Footer/>
     </div>
   );
 }
